fa94b2cdcd4cbab9edd024cd91baea0f7b39f637
jossdz <carloscorrea.dev@gmail.com>
2020-09-22 09:51:08 -0500

    deployed on heroku (samesite)

---
+++ public/build/precache-manifest.3c6de5c8ef99cc5a7def1cfabd79d930.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2afc7d7902c038cb9ef8684316c6f7d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "918a72cddc7ba5adc177",
+    "url": "/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "918a72cddc7ba5adc177",
+    "url": "/static/js/2.7d04c6f2.chunk.js"
+  },
+  {
+    "revision": "bb84f9558a70cdf960b4",
+    "url": "/static/js/main.f0f58776.chunk.js"
+  },
+  {
+    "revision": "fb47f3c02db3ce888eb4",
+    "url": "/static/js/runtime-main.dc128e9d.js"
+  }
+]);
